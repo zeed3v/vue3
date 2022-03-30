2c1ba7565ebe7658f3b21eabd3f57733e9512810
zeed3v <felipebrionese@gmail.com>
2022-03-30 09:00:34 -0300

    commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,14 @@
 app.component('footer-banco', {
+    props: ['valor', 'fecha'],
     template: /*html*/`
     <div class="bg-dark py-3 mt-2 text-white">
-        <h3> {{ texto }} - {{ cantidad }} </h3>
+        <h3> {{ texto }} - {{ valor }} </h3>
+        <p> {{fecha}} </p>
     </div>
     `,
     data() {
         return {
             texto: 'Footer de mi sitio web dinámico',
-            cantidad: 1000
         }
     }
 })
